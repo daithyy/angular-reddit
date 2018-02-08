1c5c7cd5b6464758feb4cdf3f16f3d55ea15a03f
David Brennan <david.brennan@mail.itsligo.ie>
2018-02-08 13:08:50 +0000

    Added dry articles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title ${title.value} and link: ${link.value}`);
