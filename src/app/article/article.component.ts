d3c78d9151e58b340f98936c5acbc770ebf08d64
David Brennan <david.brennan@mail.itsligo.ie>
2018-02-08 18:27:39 +0000

    Added multiple articles.

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  HostBinding, 
+  Input 
+} from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -9,13 +14,10 @@ import { Article } from './article.model';
 
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article;
+  @Input() article: Article;
 
   constructor() {
-    this.article = new Article(
-      'Angular 2',
-      'http://angular.io',
-      10);
+    // Article is populated by the Input.
    }
 
   voteUp(): boolean {
@@ -30,5 +32,4 @@ export class ArticleComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
